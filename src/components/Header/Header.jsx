4ff971275c120bac927873b65ff36642c7b29e66
codingwolf-at <atameshwari9@gmail.com>
2021-11-12 22:54:46 +0530

    feat: about developer section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <div className="header">
       <h1>Notes Keep</h1>
+      <a target="_blank" rel="noreferrer" href="https://linktr.ee/codingwolf">About Developer</a>
     </div>
   )
 }
