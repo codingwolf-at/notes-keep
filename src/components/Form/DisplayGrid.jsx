0e9337df73da69dbb2144bea19427b002f08e2d0
codingwolf-at <atameshwari9@gmail.com>
2021-11-12 21:20:42 +0530

    feat: close button on grid cards

--- src/components/Form/DisplayGrid.jsx
+++ src/components/Form/DisplayGrid.jsx
@@ -7,7 +7,8 @@ export const DisplayGrid = ({notes}) => {
         notes.length 
         ? notes.map(note => (
           <div id={note.id} className="grid-item">
-            {note.textValue}
+            <button className="item-btn close">x</button>
+            <p>{note.textValue}</p>
           </div>
         ))
         : <span className="user-msg">Notes are currently empty! <br /> (┬┬﹏┬┬)</span>
