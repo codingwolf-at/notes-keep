06d01654acd91751620dec62b4270691b95bcc01
codingwolf-at <atameshwari9@gmail.com>
2021-11-14 20:42:21 +0530

    feat: notes are now being saved in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Header, Form } from './components';
 
 function App() {
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('notes'));
+    setNotes(data);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes])
+
   return (
     <div className="App">
       <Header />
