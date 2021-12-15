807586ed3f34af92f2a2ac2f7d920ac9dce318c2
codingwolf-at <atameshwari9@gmail.com>
2021-12-15 21:34:40 +0530

    feat: added category radio buttons for input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,6 +5,7 @@ import { useNotes } from '../../hooks';
 
 export const Form = () => {
   const [input, setInput] = useState("");
+  const [radioInput, setRadioInput] = useState('Note');
   const inputElement = useRef(null);
 
   const {setNotes} = useNotes();
@@ -28,16 +29,54 @@ export const Form = () => {
       ...notes, {
         id: uuidv4(),
         textValue: input,
-        date: getDate()
+        date: getDate(),
+        type: radioInput
       }
     ]);
-    setInput("")
+    setInput('');
+  };
+
+  const radioInputHandler = (e) => {
+    setRadioInput(e.target.value);
   }
+  
   return (
     <div className="wrapper">
       <div className="form">
         <input ref={inputElement} type="text" onChange={(e) => inputHandler(e)} value={input} placeholder="Take a note..." />
         <button onClick={notesHandler}>Add to Notes</button>
+        <div className="radio-wrapper">
+          <label className='radio-label'>
+            <input 
+              type="radio" 
+              value='Note' 
+              name='type' 
+              checked={radioInput === 'Note'}
+              onChange={(e) => radioInputHandler(e)} 
+            />
+            Note
+          </label>
+          <label className='radio-label'>
+            <input 
+              type="radio" 
+              value='Reminder' 
+              name='type' 
+              checked={radioInput === 'Reminder'}
+              onChange={(e) => radioInputHandler(e)} 
+            />
+            Reminder
+          </label>
+          <label className='radio-label'>
+            <input 
+              type="radio" 
+              value='Important' 
+              name='type' 
+              checked={radioInput === 'Important'}
+              onChange={(e) => radioInputHandler(e)} 
+            />
+            Important
+          </label>
+        </div>
       </div>
     </div>
   )
