7cd4464ef144c813e01a8febaf02e102f456f5d3
codingwolf-at <atameshwari9@gmail.com>
2021-12-24 13:02:23 +0530

    feat: added routes for Reminder & Important notes page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Header } from './components';
 import { themeContext } from './contexts/theme-context';
 import './App.css';
-import { Home, Trash } from './pages';
+import { Home, Important, Reminder, Trash } from './pages';
 
 function App() {
   const {theme} = useContext(themeContext);
@@ -14,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/trash" element={<Trash />} />
+        <Route path="/reminder" element={<Reminder />} />
+        <Route path="/important" element={<Important />} />
       </Routes>
     </div>
   );
