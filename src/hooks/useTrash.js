68a5de4e59f9552857c47a1408cdcb0fb3c58ab2
codingwolf-at <atameshwari9@gmail.com>
2021-12-25 11:16:02 +0530

    fix: useContext import

--- src/hooks/useTrash.js
+++ src/hooks/useTrash.js
@@ -1,5 +1,6 @@
-import { useContext } from "react/cjs/react.development"
-import { notesContext } from "../contexts/notes-context"
+
+import { useContext } from "react";
+import { notesContext } from "../contexts/notes-context";
 
 export const useTrash = () => {
   const {trash, setTrash} = useContext(notesContext);
