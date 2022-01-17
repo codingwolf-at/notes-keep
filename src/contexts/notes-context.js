433c0534a7572c19626961eb13ea1b885cd95b89
codingwolf-at <atameshwari9@gmail.com>
2022-01-17 22:13:50 +0530

    fix: redundant code removed

--- src/contexts/notes-context.js
+++ src/contexts/notes-context.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useReducer } from "react";
 import {getDate} from "../utils";
 
 export const notesContext = createContext();
