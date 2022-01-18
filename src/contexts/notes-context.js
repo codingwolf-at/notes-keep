b85deae639ce560ed92230a79516b0be578bc01b
codingwolf-at <atameshwari9@gmail.com>
2022-01-18 13:09:47 +0530

    feat: re-implemented fetching from LocalStorage using useReducer & useEffect

--- src/contexts/notes-context.js
+++ src/contexts/notes-context.js
@@ -1,36 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import {getDate} from "../utils";
 
 export const notesContext = createContext();
 
 export const NotesProvider = ({ children }) => {
 
-  const initialState = {
-    notes: [],
-    trash: []
-  }
+  const [{notes, trash}, dispatch] = useReducer(notesReducer, {notes: [], trash: []});
 
-  const [{notes, trash}, dispatch] = useReducer(notesReducer, initialState);
+  useEffect(() => {
+    const notesData = JSON.parse(localStorage.getItem('notes'));
+    const trashData = JSON.parse(localStorage.getItem('trash'));
+    if (notesData.length > 0 || trashData.length > 0) {
+      dispatch({type: "UPDATE_FROM_LOCAL_STORAGE", payload: {notesData, trashData}})
+    } 
+  }, [])
 
-  // useEffect(() => {
-  //   const notesData = JSON.parse(localStorage.getItem('notes'));
-  //   const trashData = JSON.parse(localStorage.getItem('trash'));
-  //   if (notesData) {
-  //     setNotes(notesData);
-  //   }
-  //   if (trashData) {
-  //     setTrash(trashData);
-  //   }
-  // }, [])
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes])
 
-  // useEffect(() => {
-  //   localStorage.setItem('notes', JSON.stringify(notes));
-  // }, [notes])
-
-  // useEffect(() => {
-  //   localStorage.setItem('trash', JSON.stringify(trash));
-  // }, [trash]);
+  useEffect(() => {
+    localStorage.setItem('trash', JSON.stringify(trash));
+  }, [trash]);
 
   return (
     <notesContext.Provider value={{notes, trash, dispatch}}>
@@ -41,6 +33,11 @@ export const NotesProvider = ({ children }) => {
 
 const notesReducer = (state, action) => {
   switch(action.type) {
+    case "UPDATE_FROM_LOCAL_STORAGE":
+      return {
+        notes: action.payload.notesData,
+        trash: action.payload.trashData
+      }
     case "ADD_TO_NOTES": 
       return {
         notes: state.notes.concat({
